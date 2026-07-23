988b82cea21c384314e13439fa6b62e647fc1ac3
khatunsarbina403-source <khatunsarbina403@gmail.com>
2026-07-24 00:04:45 +0530

    Fix URL construction for Telegram API calls

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ define('API_KEY', $token);
 #functions 
 function bot($method, $datas = [])
 {
-    $url = "https://api.telegram.org/bot" . API_KEY . "j51i0phg9tje7mcgvjj9" . $method;
+    $url = "https://api.telegram.org/bot".API_KEY."/".$method;
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
